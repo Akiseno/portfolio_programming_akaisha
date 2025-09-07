d0dc429fed720287419266ffaabed1c15174a1a3
akaisha <akaisha.senoron@gmail.com>
2025-09-07 11:27:09 +0200

    introduction print

---
+++ week1/hello_world.py
@@ -0,0 +1,12 @@
+print("hello world")
+print("how are you i am programming today")
+print("i am learning python")
+print("i am akaisha")
+# Method 1: Using Unicode escape sequence
+print("\U0001F604")  # 😄
+
+# Method 2: Using the emoji directly
+print ("all colors are nice except brown")
+(
+    print("all colors are nice except brown")
+)
